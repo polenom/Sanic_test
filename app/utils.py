d286e2b03da49194f964fe6ba05c1587cdcb9b23
polenom <vitalimitsevich@yandex.by>
2022-11-01 11:00:22 +0300

    feat: add jwt, login and refresh

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,41 @@
+import datetime
 import hashlib
+import os
+
+from dotenv import load_dotenv
+from sanic_jwt_extended import JWT
+
+load_dotenv()
+SALT = os.getenv("SALT")
 
 
-
-def getPass( password: str, salt: str) -> str | None:
+def get_pass(password: str, salt: str = SALT) -> str | None:
     if password:
-        return hashlib.sha512((salt+password).encode()).hexdigest()
+        return hashlib.sha512((salt + password).encode()).hexdigest()
 
 
+def error_response(msg: str) -> dict:
+    return dict(
+        error=True,
+        message=msg
+    )
 
+
+def get_access_and_refresh_tokens(identity: str) -> tuple[str]:
+    access_token = get_access_token(identity)
+    refresh_token = get_refresh_token(identity)
+    return (access_token, refresh_token)
+
+
+def get_access_token(identity: str) -> str:
+    return JWT.create_access_token(
+        identity=identity,
+        expires_delta=datetime.timedelta(minutes=5)
+    )
+
+
+def get_refresh_token(identity: str) -> str:
+    return JWT.create_refresh_token(
+        identity=identity,
+        expires_delta=datetime.timedelta(days=1),
+    )
--- main.py
+++ main.py
@@ -1,49 +1,81 @@
+import datetime
 import os
+import json as JSON
 
 from sanic import Sanic
-from sanic.response import text, json
-from sanic_jwt import Initialize
+from sanic.response import text, json, Request
+from sanic_jwt import Initialize, protected
+from sanic_jwt_extended import JWT, refresh_jwt_required, jwt_required
+from sanic_jwt_extended.tokens import Token
 from tortoise.contrib.sanic import register_tortoise
 
 from app.models import User, Products, Bill
-from app.permission import authenticate
-from app.utils import getPass
+from app.utils import *
 from dotenv import load_dotenv
 
-load_dotenv()
 app = Sanic("My_app")
+load_dotenv()
 register_tortoise(app, db_url="postgres://sanic:sanic@0.0.0.0:2020/sanicproject", modules={"models": ["app.models"]},
                   generate_schemas=True)
-Initialize(
-    app,
-    authenticate=authenticate,
-    access_token_name="token",
-    url_prefix='/user/auth',
-    secret=os.getenv('TOKEN'))
-SALT = os.getenv("SALT")
+
+with JWT.initialize(app) as manager:
+    manager.config.secret_key = os.getenv("TOKEN")
+
+
 
 @app.get("/")
-async def hello_world(request):
+@jwt_required
+async def hello_world(request: Request, token: Token):
     print(request)
     return text("hello world")
-
-
-@app.post("user/create/<user>")
-async def create_user(request, user):
-    # await User(
-    # name = "admin",
-    # password = getPass('admin')
-    # ).save()
-    a = await User.filter(id=1)
-    print(type(a[0]))
-    # await Bill(
-    #     user = a[0],
-    #     idBill = "123131k3j21jjdlsjfl12312",
-    #     balance = 13 ).save()
-    print(type(request))
+#
+#
+@app.post("/user/create/")
+async def create_user(request):
+    await User(
+    name = "admin",
+    password = get_pass('admin')
+    ).save()
     return json({"user": "admin",
                  "error": False
                  })
+
+@app.route("/login", methods=["POST"])
+async def login(request: Request):
+    username = request.json.get("username")
+    password = request.json.get("password")
+    if not username or not password:
+        return json(error_response("Missing username or password"),
+                    status=400)
+    user = await User.filter(name=username)
+    if not user:
+        return json(error_response("User not found"),
+                    status=400)
+    if user[0].password != get_pass(password):
+        return json(error_response("Password error"),
+                    status=400)
+    access_token, refresh_token = get_access_and_refresh_tokens(user[0].name)
+    return json(
+        dict(
+            access_token=access_token,
+            refresh_token=refresh_token
+        ),
+        status=200
+    )
+
+
+@app.route("/refresh", methods=['POST'])
+@refresh_jwt_required
+async def refresh(request: Request, token: Token):
+    access_token, refresh_token = get_access_and_refresh_tokens(token.identity)
+    return json(
+        dict(
+            access_token=access_token,
+            refresh_token=refresh_token
+        ),
+        status=200
+    )
+
 
 
 if __name__ == "__main__":
